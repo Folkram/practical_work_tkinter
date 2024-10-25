b13fcf72d119d9c3979ec3a6488c80a02244a6f8
Folkram <folkram2187@gmail.com>
2024-10-25 11:50:43 +0300

    feat(tkinter_8.py): add pattern() function
    * add pattern() function to insert pattern into all fields
    * bind pattern() function to button_pattern
    * on click on button_pattern will insert pattern into all fields

--- tkinter_8.py
+++ tkinter_8.py
@@ -1,6 +1,28 @@
 # import module
 import tkinter as tk
 
+
+# functions section
+def pattern():  # insert pattern into all fields
+    text_about_yourself.delete(1.0, 'end')
+    text_about_yourself.insert(1.0, "A good and honest man \n"
+                                    "who is also very hardworking,\n"
+                                    "but that's not all! I'm also very humble.\n"
+                                    "This is your best chance not to miss someone like me!")
+    entry_name.insert(1, 'Oleg')
+    entry_password.insert(1, 'OlegIsGood')
+
+    # line-by-line formatting
+    text_about_yourself.tag_add('second', 2.0, '2.end')
+    text_about_yourself.tag_config('second', font=('Arial', round(def_font_size * 0.9)))
+    text_about_yourself.tag_add('third', 3.0, '3.end')
+    text_about_yourself.tag_config('third', font=('Arial', round(def_font_size * 0.8)))
+    text_about_yourself.tag_add('forth', 4.0, '4.end')
+    text_about_yourself.tag_config('forth', font=('Arial', round(def_font_size * 0.7)))
+    text_about_yourself.tag_add('fifth', 5.0, '5.end')
+    text_about_yourself.tag_config('fifth', font=('Arial', round(def_font_size * 0.6)))
+
+
 # root window
 root = tk.Tk()
 root.title('Form')
@@ -19,7 +41,7 @@ label_name = tk.Label(main_frame, text='Enter your name')
 entry_name = tk.Entry(main_frame)
 label_password = tk.Label(main_frame, text='Enter your password')
 entry_password = tk.Entry(main_frame, show='*')
-button_pattern = tk.Button(main_frame, text='Pattern')
+button_pattern = tk.Button(main_frame, text='Pattern', command=pattern)
 button_show_password = tk.Button(main_frame, text='Show password')
 button_post = tk.Button(main_frame, text='Send')
 button_clear = tk.Button(main_frame, text='Clear')
