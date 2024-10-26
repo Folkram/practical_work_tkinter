9ed85106768bb3fb2a307712d985d4e6e2505078
Folkram <folkram2187@gmail.com>
2024-10-26 17:11:48 +0300

    feat(tkinter_9.py): add scrollbar
    * add scrollbar for text field

--- tkinter_9.py
+++ tkinter_9.py
@@ -9,12 +9,14 @@ root.geometry('1300x925')
 # widgets section
 main_frame = tk.Frame()
 text = tk.Text(main_frame)
+scroll = tk.Scrollbar(main_frame, orient='vertical', command=text.yview)
 
 # widgets style
-text.config(font=("Comic Sans MS", 20), wrap='word')
+text.config(font=("Comic Sans MS", 20), wrap='word', yscrollcommand=scroll.set)
 
 # placement of widgets
 main_frame.pack(fill='both', expand=1)
+scroll.pack(side='right', fill='y')
 text.pack(fill='both', expand=1)
 
 root.mainloop()  # display window
