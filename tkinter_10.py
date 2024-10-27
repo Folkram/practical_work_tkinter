71df54712e656ee3b114d8aa791cc5bbefa2c989
Folkram <folkram2187@gmail.com>
2024-10-27 21:45:03 +0300

    style(tkinter_10.py): fix text errors

--- tkinter_10.py
+++ tkinter_10.py
@@ -4,23 +4,23 @@ from tkinter import messagebox as mb
 
 
 # function section
-def show_translate():  # display messagebox with translate
+def show_translation():  # display messagebox with translation
     if listbox.curselection() == ():
         mb.showerror('Error', 'None of the options are selected')
     else:
-        translate = options_translate[listbox.curselection()[0]]  # get translation for current word
-        mb.showinfo(f'{listbox.get(listbox.curselection()[0])}', f'Word translation: {translate}')
+        translation = options_translation[listbox.curselection()[0]]  # get translation for current word
+        mb.showinfo(f'{listbox.get(listbox.curselection()[0])}', f'Word translation: {translation}')
 
 
 # root window
 root = tk.Tk()
-root.title('Translate')
+root.title('Translator')
 root.geometry('280x325')
 
 # widgets section
 listbox = tk.Listbox(root)
 scroll = tk.Scrollbar(root, orient='vertical', command=listbox.yview)
-btn_translate = tk.Button(root, text='Translate', command=show_translate)
+btn_translate = tk.Button(root, text='Translate', command=show_translation)
 
 # widgets style
 listbox.config(yscrollcommand=scroll.set, font=('Arial', 18), width=20)
@@ -34,9 +34,9 @@ btn_translate.grid(columnspan=2, row=1, sticky='news')
 # options for listbox
 options = ['artichoke', 'eggplant', 'bobs', 'ginger', 'marrow', 'cabbage', 'cauliflower', 'broccoli', 'brussels sprout',
            'apricot', 'avocado', 'pine apple', 'orange', 'watermelon', 'banana', 'blackberry']
-# translate for options
-options_translate = ['артишок', 'баклажан', 'бобы', 'имбирь', 'кабачок', 'капуста', 'цветная капуста', 'брокколи',
-                     'брюссельская капуста', 'абрикос', 'авокадо', 'ананас', 'апельсин', 'арбуз', 'банан', 'ежевика']
+# translation for options
+options_translation = ['артишок', 'баклажан', 'бобы', 'имбирь', 'кабачок', 'капуста', 'цветная капуста', 'брокколи',
+                       'брюссельская капуста', 'абрикос', 'авокадо', 'ананас', 'апельсин', 'арбуз', 'банан', 'ежевика']
 # insert options into listbox
 i = 0  # counter
 for option in options:
