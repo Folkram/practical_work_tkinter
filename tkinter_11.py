a0398841d9d8344e5dd65414539c4033a40fce67
Folkram <folkram2187@gmail.com>
2024-10-28 14:25:47 +0300

    feat(tkinter_11.py): add greeting frame
    * add greeting_frame to display greeting
    * add btn_frame to place the button

--- tkinter_11.py
+++ tkinter_11.py
@@ -11,4 +11,17 @@ color1 = '#7df0ff'
 color2 = '#00063d'
 color3 = '#010d7a'
 
+# greeting frame
+# widgets section
+greeting_frame = tk.Frame(root)
+btn_frame = tk.Frame(root)
+
+# widgets style
+greeting_frame.config(bg=color1)
+btn_frame.config(bg=color1)
+
+# placement of widgets
+greeting_frame.pack(fill='both', expand=1)
+btn_frame.pack(fill='both', side='bottom')
+
 root.mainloop()  # display window
