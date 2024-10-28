103c95890d332b87c5c752fadc3e6e1a61130303
Folkram <folkram2187@gmail.com>
2024-10-28 14:29:15 +0300

    feat(tkinter_11.py): add class MyLabel
    * add class MyLabel to create label

--- tkinter_11.py
+++ tkinter_11.py
@@ -1,6 +1,15 @@
 # import module
 import tkinter as tk
 
+
+# class section
+class MyLabel:  # create label
+    def __init__(self, m_root, m_text, m_bg, m_fg, m_font, m_wraplength, m_justify, m_padx, m_pady):
+        self.label = tk.Label(m_root, text=m_text, bg=m_bg, fg=m_fg, wraplength=m_wraplength, justify=m_justify,
+                              font=m_font)
+        self.label.pack(padx=m_padx, pady=m_pady)
+
+
 # root window
 root = tk.Tk()
 root.title('Quiz about penguins')
