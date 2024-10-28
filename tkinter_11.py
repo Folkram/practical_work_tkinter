1af847e2336b9e13d82cc5ba557dc6db55180d1a
Folkram <folkram2187@gmail.com>
2024-10-28 14:18:23 +0300

    feat(tkinter_11.py): add root window
    * add a root window
    * add a colors setting

---
+++ tkinter_11.py
@@ -0,0 +1,14 @@
+# import module
+import tkinter as tk
+
+# root window
+root = tk.Tk()
+root.title('Quiz about penguins')
+root.geometry('350x500')
+root.resizable(width=False, height=False)
+# colors
+color1 = '#7df0ff'
+color2 = '#00063d'
+color3 = '#010d7a'
+
+root.mainloop()  # display window
