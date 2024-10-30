a419ba1f6578e04a2da0acce8ddfbbb4714c786d
Folkram <folkram2187@gmail.com>
2024-10-30 13:07:02 +0300

    feat(tkinter_12.py): add functions
    * add fullscreen_switch() function to switch fullscreen mode
    * add switch_theme() function to switch between dark and light themes

--- tkinter_12.py
+++ tkinter_12.py
@@ -1,6 +1,23 @@
 # import module
 import tkinter as tk
 
+
+# function section
+def fullscreen_switch():
+    global fullscreen
+    fullscreen = not fullscreen
+    root.overrideredirect(fullscreen)
+
+
+def switch_theme():
+    global dark_theme
+    dark_theme = not dark_theme
+    if dark_theme:
+        root.config(bg='#212121')
+    else:
+        root.config(bg='#f2f2f2')
+
+
 # root window
 root = tk.Tk()
 root_width = root.winfo_screenwidth()  # width user screen
@@ -9,6 +26,7 @@ root.geometry(f'{root_width}x{root_height}')
 root.resizable(False, False)  # not resize
 root.title('Program')
 fullscreen = False
+dark_theme = False
 
 # widget section
 main_menu = tk.Menu(root)  # создание основного меню
@@ -28,8 +46,8 @@ main_menu.add_cascade(label='Help', menu=info_menu)  # add an item from the subm
 
 config_menu = tk.Menu(main_menu, tearoff=0)  # submenu
 # add items to a submenu
-config_menu.add_command(label='Switch theme')
-config_menu.add_command(label='Fullscreen mode')
+config_menu.add_command(label='Switch theme', command=switch_theme)
+config_menu.add_command(label='Fullscreen mode', command=fullscreen_switch)
 config_menu.add_separator()  # add separator
 config_menu.add_command(label='Exit', command=lambda: root.destroy())
 main_menu.add_cascade(label='Setting', menu=config_menu)  # add an item from the submenu
