8fde9340baa2a1dbdc0efd067e691058332a2f92
Folkram <folkram2187@gmail.com>
2024-10-31 12:10:39 +0300

    feat(tkinter_13.py): add home page
    * add a title and information to the home page
    * add to_home function to switch to the home page
    * bind to_home and 'Home' in the menu

--- tkinter_13.py
+++ tkinter_13.py
@@ -1,6 +1,13 @@
 # import module
 import tkinter as tk
 
+
+# functions section
+def to_home():  # switch to home page
+    # open home_frame
+    home_frame.pack(fill='both', expand=1)
+
+
 # root window
 root = tk.Tk()
 root.title('Result')
@@ -11,7 +18,7 @@ fullscreen = False
 main_menu = tk.Menu(root)
 root.config(menu=main_menu)
 
-main_menu.add_command(label='Home')  # go to home page
+main_menu.add_command(label='Home', command=to_home)  # go to home page
 
 # settings
 options = tk.Menu(root, tearoff=0)
@@ -34,5 +41,21 @@ tasks.add_command(label='Task №3')
 tasks.add_command(label='Task №4')
 tasks.add_command(label='Task №5')
 tasks.add_command(label='Task №6')
+
+# home page
+# widgets section
+home_frame = tk.Frame(root)
+home_title = tk.Label(home_frame, text='Welcome!')
+home_label_info = tk.Label(home_frame,
+                           text='Here you can select one of the tasks in the top menu and look at its functionality')
+
+# widgets style
+home_title.config(font=('home_frame', 20))
+home_label_info.config(font=('Arial', 11))
+
+# widgets layout
+home_frame.pack(fill='both', expand=1)
+home_title.pack(fill='both', pady=(25, 0))
+home_label_info.pack()
 
 root.mainloop()
