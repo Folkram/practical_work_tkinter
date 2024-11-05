a92909d67bec0c91c2bf586a973966afc0634fd8
Folkram <folkram2187@gmail.com>
2024-11-05 15:41:37 +0300

    feat(tkinter_15.py): add window
    * add close_btn to close window
    * add event_label to display pressed button or other event

---
+++ tkinter_15.py
@@ -0,0 +1,24 @@
+# import module
+import tkinter as tk
+
+# root window
+root = tk.Tk()
+root.title('Events')
+root_width = root.winfo_screenwidth()
+root_height = root.winfo_screenheight()
+root_font = ('Arial', round(root_width/50), 'bold')
+root.geometry(f'{root_width}x{root_height}')
+root.overrideredirect(True)
+root.config(bg='black')
+
+# widget section
+# button to close window
+close_btn = tk.Button(root, text='EXIT', font=('Arial', round(root_width/150), 'bold'), bg='black', fg='white',
+                      border=0, command=lambda: root.destroy())
+close_btn.pack(anchor='ne')
+
+# label to display pressed button or other event
+event_label = tk.Label(root, text='Track events', font=root_font, bg='black', fg='white')
+event_label.pack(fill='both', expand=1)
+
+root.mainloop()  # display window
