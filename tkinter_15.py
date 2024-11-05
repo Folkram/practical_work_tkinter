22d863468b552fa2415ebeae36a1b320992ad234
Folkram <folkram2187@gmail.com>
2024-11-05 15:43:47 +0300

    feat(tkinter_15.py): add events
    * add events that will display on label

--- tkinter_15.py
+++ tkinter_15.py
@@ -28,4 +28,16 @@ close_btn.pack(anchor='ne')
 event_label = tk.Label(root, text='Track events', font=root_font, bg='black', fg='white')
 event_label.pack(fill='both', expand=1)
 
+# events
+event_label.bind('<Motion>', lambda e, b='Mouse motion', c='red': show_pressed(e, b, c))
+event_label.bind('<Button-1>', lambda e, b='LMB', c='yellow': show_pressed(e, b, c))
+event_label.bind('<Button-2>', lambda e, b='MMB', c='blue': show_pressed(e, b, c))
+event_label.bind('<Button-3>', lambda e, b='RMB', c='green': show_pressed(e, b, c))
+root.bind('f', lambda e, b='F', c='gray': show_pressed(e, b, c))
+root.bind('1987', lambda e, b='FNaF', c='purple': show_pressed(e, b, c))
+root.bind('<Return>', lambda e, b='Enter', c='pink': show_pressed(e, b, c))
+root.bind('<Escape>', lambda e, b='Esc', c='orange': show_pressed(e, b, c))
+root.bind('<Control-Return>', lambda e, b='Ctrl+Enter', c='lime': show_pressed(e, b, c))
+root.bind('<Control-Alt-Return>', lambda e, b='Ctrl+Alt+Enter', c='cyan': show_pressed(e, b, c))
+
 root.mainloop()  # display window
