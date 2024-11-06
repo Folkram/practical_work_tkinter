57b7e932355c0bace757e339fe0309fd3193a71c
Folkram <folkram2187@gmail.com>
2024-11-06 18:37:04 +0300

    feat(tkinter17.py): add tank movement
    * add tank movement to up, down, left and right

--- tkinter17.py
+++ tkinter17.py
@@ -16,4 +16,9 @@ barrel = field.create_rectangle(397.5, 275, 402.5, 230, fill='#042b00', tags='ta
 left_track = field.create_rectangle(360, 270, 372, 330, width=2, outline='black', fill='#212121', tags='tank')
 right_track = field.create_rectangle(428, 270, 440, 330, width=2, outline='black', fill='#212121', tags='tank')
 
+field.bind('<Up>', lambda e: field.move('tank', 0, -10))
+field.bind('<Down>', lambda e: field.move('tank', 0, 10))
+field.bind('<Left>', lambda e: field.move('tank', -10, 0))
+field.bind('<Right>', lambda e: field.move('tank', 10, 0))
+
 root.mainloop()  # display window
