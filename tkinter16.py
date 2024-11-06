ecb0edd05f2cd8ec5b0636cd7c1c32a2b96f071a
Folkram <folkram2187@gmail.com>
2024-11-06 12:22:06 +0300

    feat(tkinter_16.py): add purple guy
    * add purple guy's head that display on canvas

--- tkinter16.py
+++ tkinter16.py
@@ -12,4 +12,20 @@ canvas = tk.Canvas(root)
 canvas.config(bg='black', width=400, height=500)
 canvas.pack()
 
+# purple guy
+# head
+canvas.create_rectangle(75, 35, 300, 75, fill='#6f4b95', width=0)
+canvas.create_rectangle(50, 75, 350, 400, fill='#6f4b95', width=0)
+canvas.create_rectangle(75, 400, 300, 450, fill='#6f4b95', width=0)
+
+# eyes
+canvas.create_rectangle(50, 200, 150, 250, fill='black', width=0)
+canvas.create_rectangle(200, 200, 300, 250, fill='black', width=0)
+canvas.create_rectangle(50, 175, 100, 250, fill='white', width=0)
+canvas.create_rectangle(200, 175, 250, 250, fill='white', width=0)
+
+# smile
+canvas.create_rectangle(65, 320, 325, 340, fill='black', width=0)
+canvas.create_rectangle(90, 325, 285, 425, fill='black', width=0)
+
 root.mainloop()  # display window
