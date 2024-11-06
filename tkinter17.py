10891feddd3405dc9ef42577c327aae065f3ef7c
Folkram <folkram2187@gmail.com>
2024-11-06 20:25:06 +0300

    style(tkinter17.py): small change
    * add comments
    * delete variable name
    * change margins

--- tkinter17.py
+++ tkinter17.py
@@ -3,8 +3,9 @@ import tkinter as tk
 
 
 # function section
-def aim(event):
-    field.delete('aim')
+def aim(event):  # create aim on current cursor location
+    field.delete('aim')  # delete old aim
+    # create new aim
     field.create_rectangle(event.x-5, event.y+5, event.x+5, event.y-5, width=0, tags='aim')  # hitbox
     field.create_line(event.x, event.y+15, event.x, event.y-15, width=5, fill='black', tags='aim')  # horizontal line
     field.create_line(event.x-15, event.y, event.x+15, event.y, width=5, fill='black', tags='aim')  # vertical line
@@ -39,15 +40,16 @@ root.title('Tanks')
 root.geometry('800x600')
 root.config(cursor="none")  # remove cursor
 
+# widget style
 field = tk.Canvas(root, border=0, highlightthickness=0, bg='#383838', width=800, height=600)
-field.focus_set()
+field.focus_set()  # set focus to work with bind
 field.pack()
 
 # tank
-body = field.create_rectangle(375, 275, 425, 325, fill='green', width=5, outline='#042b00', tags='tank')
-barrel = field.create_rectangle(397.5, 275, 402.5, 230, fill='#042b00', tags='tank')
-left_track = field.create_rectangle(360, 270, 372, 330, width=2, outline='black', fill='#212121', tags='tank')
-right_track = field.create_rectangle(428, 270, 440, 330, width=2, outline='black', fill='#212121', tags='tank')
+field.create_rectangle(375, 275, 425, 325, fill='green', width=5, outline='#042b00', tags='tank')  # body (hitbox)
+field.create_rectangle(397.5, 275, 402.5, 230, fill='#042b00', tags='tank')  # barrel
+field.create_rectangle(360, 270, 372, 330, width=2, outline='black', fill='#212121', tags='tank')  # left track
+field.create_rectangle(428, 270, 440, 330, width=2, outline='black', fill='#212121', tags='tank')  # right track
 
 # tank movement
 field.bind('<Up>', lambda e: field.move('tank', 0, -10))
@@ -55,9 +57,7 @@ field.bind('<Down>', lambda e: field.move('tank', 0, 10))
 field.bind('<Left>', lambda e: field.move('tank', -10, 0))
 field.bind('<Right>', lambda e: field.move('tank', 10, 0))
 
-# aim
-field.bind('<Motion>', aim)
-
+field.bind('<Motion>', aim)  # set aim
 field.tag_bind('aim', '<Button-1>', shoot)  # shooting function
 
 root.mainloop()  # display window
