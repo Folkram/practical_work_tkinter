b3deb0a70558343b7b540fab3dbc788226df697b
Folkram <folkram2187@gmail.com>
2024-11-06 18:23:23 +0300

    feat(tkinter17.py): add tank
    * add tank
    * change canvas color

--- tkinter17.py
+++ tkinter17.py
@@ -6,7 +6,14 @@ root = tk.Tk()
 root.title('Tanks')
 root.geometry('800x600')
 
-field = tk.Canvas(root, border=0, highlightthickness=0, bg='black', width=800, height=600)
+field = tk.Canvas(root, border=0, highlightthickness=0, bg='#383838', width=800, height=600)
+field.focus_set()
 field.pack()
+
+# tank
+body = field.create_rectangle(375, 275, 425, 325, fill='green', width=5, outline='#042b00', tags='tank')
+barrel = field.create_rectangle(397.5, 275, 402.5, 230, fill='#042b00', tags='tank')
+left_track = field.create_rectangle(360, 270, 372, 330, width=2, outline='black', fill='#212121', tags='tank')
+right_track = field.create_rectangle(428, 270, 440, 330, width=2, outline='black', fill='#212121', tags='tank')
 
 root.mainloop()  # display window
