bd1a4ee5f3a0cd78454eba4043968f549f51f3d1
Folkram <folkram2187@gmail.com>
2024-11-06 19:09:43 +0300

    feat(tkinter17.py): add aim
    * remove cursor
    * add aim instead of cursor

--- tkinter17.py
+++ tkinter17.py
@@ -1,10 +1,19 @@
 # import module
 import tkinter as tk
 
+
+# function section
+def aim(event):
+    field.delete('aim')
+    field.create_line(event.x, event.y+15, event.x, event.y-15, width=5, fill='black', tags='aim')
+    field.create_line(event.x-15, event.y, event.x+15, event.y, width=5, fill='black', tags='aim')
+
+
 # root window
 root = tk.Tk()
 root.title('Tanks')
 root.geometry('800x600')
+root.config(cursor="none")  # remove cursor
 
 field = tk.Canvas(root, border=0, highlightthickness=0, bg='#383838', width=800, height=600)
 field.focus_set()
@@ -16,9 +25,13 @@ barrel = field.create_rectangle(397.5, 275, 402.5, 230, fill='#042b00', tags='ta
 left_track = field.create_rectangle(360, 270, 372, 330, width=2, outline='black', fill='#212121', tags='tank')
 right_track = field.create_rectangle(428, 270, 440, 330, width=2, outline='black', fill='#212121', tags='tank')
 
+# tank movement
 field.bind('<Up>', lambda e: field.move('tank', 0, -10))
 field.bind('<Down>', lambda e: field.move('tank', 0, 10))
 field.bind('<Left>', lambda e: field.move('tank', -10, 0))
 field.bind('<Right>', lambda e: field.move('tank', 10, 0))
+
+# aim
+field.bind('<Motion>', aim)
 
 root.mainloop()  # display window
