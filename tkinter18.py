70da7f55228881c3e4f07c84477bd0fbc9791aa6
Folkram <folkram2187@gmail.com>
2024-11-07 12:44:46 +0300

    feat(tkinter18.py): add figure
    * the figure is located in the upper left corner
    * the figure consists of six parts  of different colors
    * the size of the figure depends on the size of the window

--- tkinter18.py
+++ tkinter18.py
@@ -13,4 +13,17 @@ root.attributes("-fullscreen", True)  # fullscreen
 can = tk.Canvas(root, bg='black', border=0, highlightthickness=0, width=root_width, height=root_height)
 can.pack()
 
+# figure
+# size
+fig_width = root_width/9
+fig_height = root_width/12
+
+# parts (figure consists of six parts)
+can.create_rectangle(0, 0, fig_width/6, fig_height, width=0, fill='lime', tags='figure')  # part 1
+can.create_rectangle(fig_width/6, 0, fig_width*2/6, fig_height, width=0, fill='yellow', tags='figure')  # part 2
+can.create_rectangle(fig_width*2/6, 0, fig_width*3/6, fig_height, width=0, fill='red', tags='figure')  # part 3
+can.create_rectangle(fig_width*3/6, 0, fig_width*4/6, fig_height, width=0, fill='blue', tags='figure')  # part 4
+can.create_rectangle(fig_width*4/6, 0, fig_width*5/6, fig_height, width=0, fill='purple', tags='figure')  # part 5
+can.create_rectangle(fig_width*5/6, 0, fig_width, fig_height, width=0, fill='pink', tags='figure')  # part 6
+
 root.mainloop()  # display window
