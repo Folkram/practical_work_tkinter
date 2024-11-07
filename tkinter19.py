c78840f3a924faedb2bc2d6c63498a78143d1a92
Folkram <folkram2187@gmail.com>
2024-11-07 19:07:12 +0300

    feat(tkinter18.py): add root window
    * the root window opens in 800x600 format
    * the root window is dark gray

---
+++ tkinter19.py
@@ -0,0 +1,10 @@
+# import module
+import tkinter as tk
+
+# root window
+root = tk.Tk()
+root.title('Gallery')
+root.geometry('800x600')
+root.config(bg='#2b2b2b')
+
+root.mainloop()  # display window
