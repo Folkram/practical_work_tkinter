65d956f4f30247913f2aa8c04efb5bfa1630bacd
Folkram <folkram2187@gmail.com>
2024-11-07 19:20:18 +0300

    feat(tkinter19.py): add canvas and button
    * photos will be displayed in the canvas
    * photos will be opened using the open_btn

--- tkinter19.py
+++ tkinter19.py
@@ -5,6 +5,16 @@ import tkinter as tk
 root = tk.Tk()
 root.title('Gallery')
 root.geometry('800x600')
-root.config(bg='#2b2b2b')
+root.config(bg='#0d0d0d')
+
+# widget section
+# canvas
+can = tk.Canvas(root, bg='#2b2b2b',  border=0, highlightthickness=0, width=800, height=550)
+can.pack()
+
+# button
+open_btn = tk.Button(root, text='Open Photo', bg='#2b2b2b', font=('Arial', 17), bd=0, fg='#c9c9c9',
+                     activebackground='#0d0d0d', activeforeground='#2b2b2b')
+open_btn.pack(fill='both', expand=1, padx=5, pady=5)
 
 root.mainloop()  # display window
