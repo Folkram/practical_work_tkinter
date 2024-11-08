f0e171a33a83fcf11b36babadde85f2904122d5c
Folkram <folkram2187@gmail.com>
2024-11-08 11:50:56 +0300

    feat(tkinter20.py): add title and main_frame
    * add title and main_frame
    * split the main_frame into columns of the same size

--- tkinter20.py
+++ tkinter20.py
@@ -18,4 +18,18 @@ for i in range(6):  # 6 columns of same size
 root.rowconfigure(0, minsize=100)
 root.rowconfigure(1, minsize=root_height-100)
 
+# widget section
+title = tk.Label(root, text='BATTLE SIMULATION', bg='black', fg=main_color, font=('Fallout Regular', 40))
+title.grid(column=1, columnspan=5, row=0, sticky='NEWS')
+
+main_frame = tk.Label(root, bg=main_color)
+main_frame.grid(columnspan=6, row=1, sticky='NEWS')
+
+# split the main_frame into columns of the same size
+for i in range(6):
+    main_frame.columnconfigure(i, minsize=round(root_wight/6))
+# split the main_frame into rows of the same size
+for i in range(10):
+    main_frame.rowconfigure(i, minsize=round((root_height - 100) / 10))
+
 root.mainloop()  # display window
