3f18ee261a498a82371c5e8272db29c24bb7f5e2
Folkram <folkram2187@gmail.com>
2024-11-20 09:41:13 +0300

    feat(tkinter22.1.py): add canvas and diagram consisting of fourth columns
    * add a canvas to display the created diagrams
    * add a diagram consisting of fourth rectangles various size

--- tkinter22.1.py
+++ tkinter22.1.py
@@ -6,4 +6,15 @@ root = tk.Tk()
 root.title('Diagrams')
 root.geometry('800x600')
 
+# widget section
+can = tk.Canvas(root, width=800, height=600, border=0, highlightthickness=0)
+can.pack()
+
+# diagram
+# columns
+can.create_rectangle(75, 500, 175, 150, fill='blue', width=3, outline='#032773')  # first column
+can.create_rectangle(225, 500, 325, 350, fill='yellow', width=3, outline='#736b03')  # second column
+can.create_rectangle(375, 500, 475, 250, fill='green', width=3, outline='#147303')  # third column
+can.create_rectangle(525, 500, 625, 450, fill='red', width=3, outline='#730303')  # fourth column
+
 root.mainloop()  # display window
